b482db00984cc4279b75e077f541b96775cedb44
Harmaton <njagiiharmaton@gmail.com>
2024-01-03 19:08:04 +0300

    badges

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,19 @@
-import  LightningBolt, { Lightbulb } from "lucide-react"; // Assuming you have an appropriate icon library
+import { Lightbulb } from "lucide-react";
+import { Badge, badgeVariants } from "@/components/ui/badge"
+import Link from "next/link";
+
 
 const Header = () => {
   return (
-    <header className=" p-6 flex items-center">
-      <h1 className="text-4xl font-fancy font-extrabold">Our Core Competencies</h1>
-      <Lightbulb className="w-8 h-8 ml-4" />
+    <header className="p-6 flex items-center">
+      <div className="flex ml-4 space-x-10">
+      <Badge variant="outline">Enseñanza por competencia <Lightbulb className="w-3 h-3 ml-2 text-red-500" /> </Badge>
+       <Badge variant="outline">Rápido y confiable<Lightbulb className="w-3 h-3 ml-2 " /> </Badge>
+       <Badge variant="outline">Últimos datos de Pervian <Lightbulb className="w-3 h-3 ml-2 text-green-500" /> </Badge>
+      </div>
     </header>
   );
 };
+
 
 export default Header;
