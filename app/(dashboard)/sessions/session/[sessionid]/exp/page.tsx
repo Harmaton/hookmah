f3891e469b55d0182dbec1f054ee7c98e4475739
Harmaton <njagiiharmaton@gmail.com>
2024-02-20 14:31:26 +0300

    session expirience generation page

---
+++ app/(dashboard)/sessions/session/[sessionid]/exp/page.tsx
@@ -0,0 +1,11 @@
+const ExperirnceGeneratePage = async ({
+    params,
+  }: {
+    params: { sessionid: string };
+  }) => {
+
+
+    return(
+        <div>extract expression items</div>
+    )
+  }
